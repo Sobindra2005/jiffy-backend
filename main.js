4648fa528a9adb7a270dd8c37bf5c10181da3be7
Sudarshan Pokhrel <sudarshan.pokhrel2k62@gmail.com>
2024-12-30 21:45:17 +0545

    feat: refactor food controller methods for improved readability and performance; add bulk restaurant and item creation

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
